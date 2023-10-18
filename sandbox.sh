97bc945ab7f11f11bb151cd30ad2743844517836
Zou Xin <dev@zouxin.net>
2023-10-18 11:02:27 +0800

    fix: use rbenv instead of tool package (#6)
    * fix: use rbenv instead of tool package
    
    * use env field
    
    ---------
    
    Co-authored-by: Sin <sin2624@gmail.com>

--- sandbox.sh
+++ sandbox.sh
@@ -13,6 +13,12 @@ sudo apt-get install libgmp3-dev -y
 echo '== Install mysql client =='
 sudo apt-get install mysql-client libmysqlclient-dev -y
 
+echo '== Install ruby via rbenv =='
+curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer | bash
+grep -qxF 'eval "$(/home/owner/.rbenv/bin/rbenv init - bash)"' ~/.bashrc || echo 'eval "$(/home/owner/.rbenv/bin/rbenv init - bash)"' >> ~/.bashrc
+eval "$(/home/owner/.rbenv/bin/rbenv init - bash)"
+rbenv install
+
 echo '== Install latest rails =='
 gem install rails
 
